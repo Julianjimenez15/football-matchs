cb3f945a41eee2d6eeb3192f77a50074a1ea3e97
Julian Jimenez <julianjimenez2128@gmail.com>
2024-06-23 21:55:44 -0500

    feat(components):  add Navbar component

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,52 @@
+import React, { Component } from "react";
+
+class Navbar extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      isNavOpen: false, // Estado para controlar si el menú está abierto
+    };
+  }
+
+  // Función para alternar el estado del menú
+  toggleNav = () => {
+    this.setState({ isNavOpen: !this.state.isNavOpen });
+  };
+
+  render() {
+    return (
+      <nav className="navbar">
+        <div className="navbar-container">
+          <div className="navbar-logo">
+            <a href="/">Logo</a>
+          </div>
+          <div
+            className={`navbar-links ${this.state.isNavOpen ? "active" : ""}`}
+          >
+            <ul>
+              <li>
+                <a href="/">Inicio</a>
+              </li>
+              <li>
+                <a href="/">Noticias</a>
+              </li>
+              <li>
+                <a href="/">Acerca de</a>
+              </li>
+              <li>
+                <a href="/">Contacto</a>
+              </li>
+            </ul>
+          </div>
+          <div className="navbar-toggle" onClick={this.toggleNav}>
+            <i
+              className={this.state.isNavOpen ? "fas fa-times" : "fas fa-bars"}
+            ></i>
+          </div>
+        </div>
+      </nav>
+    );
+  }
+}
+
+export default Navbar;
